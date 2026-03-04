55ca4a1e90fb04e75a15bae5c92968a263c35ff9
Ivan Ni <in2174@nyu.edu>
2026-03-04 15:25:31 -0800

    Remove merge scripts: delete fix_merge.sh and resolve_merge.py

--- fix_merge.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /home/pi/team_daniel_jo_souta/NYU_ROB_UY_2004
-export GIT_EDITOR=true
-git commit --no-edit --allow-empty -m "Merge upstream/main into local branch"
-status=$?
-echo "Merge completed with status: $status"
